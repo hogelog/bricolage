56b40978108d10c852129a7bcf87957bc11aa696
komuro.sunao <komuro.sunao@mwed.co.jp>
2016-05-10 10:59:41 +0900

    Exec without bundler environment variables

--- jobclass/exec.rb
+++ jobclass/exec.rb
@@ -7,7 +7,8 @@ JobClass.define('exec') {
     script.task(params.generic_ds) {|task|
       task.action(params['args'].join(' ')) {|ds|
         ds.logger.info '[CMD] ' + params['args'].join(' ')
-        system(*params['args'])
+        except_bundler_env = Hash[%w(BUNDLE_BIN_PATH BUNDLE_GEMFILE GEM_HOME GEM_PATH RUBYLIB RUBYOPT).map {|k| [k, nil] }]
+        system(except_bundler_env, *params['args'])
         st = $?
         ds.logger.info "status=#{st.exitstatus}"
         JobResult.for_bool(st.exitstatus == 0)
